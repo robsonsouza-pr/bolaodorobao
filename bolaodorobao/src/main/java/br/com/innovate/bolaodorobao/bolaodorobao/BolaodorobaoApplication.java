ff94ce81319b94bdff5cbfaf34fa37fb4719b242
Robson dos Santos Souza <robssouza@apoioprodesp@sp.gov.br>
2018-07-20 09:05:41 -0300

    Commit inicial

---
+++ bolaodorobao/src/main/java/br/com/innovate/bolaodorobao/bolaodorobao/BolaodorobaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.innovate.bolaodorobao.bolaodorobao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaodorobaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaodorobaoApplication.class, args);
+	}
+}
---
+++ bolaodorobao/src/test/java/br/com/innovate/bolaodorobao/bolaodorobao/BolaodorobaoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.innovate.bolaodorobao.bolaodorobao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BolaodorobaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
